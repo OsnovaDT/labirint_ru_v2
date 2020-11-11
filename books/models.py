927812f35621f291c078db78a6c2308d2e146792
Evgeny Stepanov <maestrojane@gmail.com>
2020-11-11 15:54:53 +0300

    add model Author

--- books/models.py
+++ books/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Book author"""
+
+    # Used for book description
+    name = models.CharField(
+        'Имя автора',
+        help_text='Имя автора, указываемое в описании книги',
+        max_length=100,
+        unique=True,
+    )
+
+    # Used for author description
+    full_name = models.CharField(
+        'Полное имя автора',
+        max_length=100,
+        unique=True,
+    )
+    info = models.TextField(
+        'Информация об авторе',
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(
+        'Фотография автора',
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['name']
+        db_table = 'author'

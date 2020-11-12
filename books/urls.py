1389a9ced6be8ec53dc69498c1aa63cbfaecc6c5
Evgeny Stepanov <maestrojane@gmail.com>
2020-11-12 10:53:34 +0300

    add author info

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import (
+    BookListView, BookDetailView, AuthorListView
+)
 
 
 app_name = 'books'
@@ -9,10 +11,17 @@ urlpatterns = [
     # Index page with all books
     path('', BookListView.as_view(), name='index'),
 
-    # Book detail
+    # Book detail info
     path(
         'book/<int:pk>/',
         BookDetailView.as_view(),
         name='book_detail_info'
-    )
+    ),
+
+    # Author info
+    path(
+        'author/<int:author_id>/',
+        AuthorListView.as_view(),
+        name='author_info'
+    ),
 ]
--- books/views.py
+++ books/views.py
@@ -13,7 +13,7 @@ class BookListView(LoginRequiredMixin, ListView):
     """Class with all books which used on index page"""
     
     template_name = 'books/index.html'
-    paginate_by = 2
+    paginate_by = 1
     context_object_name = 'books'
     queryset = Book.objects.prefetch_related(
         'authors', 'publishing_house', 'episode', 'genres'
@@ -21,5 +21,31 @@ class BookListView(LoginRequiredMixin, ListView):
 
 
 class BookDetailView(DetailView):
+    """Class with book detail info"""
+
     model = Book
     template_name = 'books/book_detail.html'
+
+
+class AuthorListView(ListView):
+    """Class with all author's books"""
+
+    template_name = 'books/author_books.html'
+    paginate_by = 2
+    context_object_name = 'books'
+    
+    def get_queryset(self):
+        return Book.objects.filter(
+            authors=self.kwargs['author_id']
+        ).prefetch_related(
+            'authors', 'genres',
+            'publishing_house', 'episode'
+        )
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['author'] = Author.objects.get(
+            pk=self.kwargs['author_id']
+        )
+
+        return context
